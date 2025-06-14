422b8df3ec4b4968a51c51bd556cfa0efa61360a
Rishabh anand <naveen.parasi@gmail.com>
2025-06-14 16:49:37 +0530

    added  category validator

---
+++ src/category/category-router.ts
@@ -0,0 +1,7 @@
+import { CategoryController } from './category-controller';
+import express from 'express';
+import categoryValidator from './category-validator';
+const router = express.Router();
+const categoryController = new CategoryController();
+
+router.post('/', categoryValidator, categoryController.create);
